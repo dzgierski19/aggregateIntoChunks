d04bbdc9c4e855b481069d71f5ca4a7a3f507cc3
dzgierski19 <dzgierskigit@gmail.com>
2023-06-30 12:36:25 +0200

    review branch

--- src/test/aggregateIntoChunks.test.ts
+++ src/test/aggregateIntoChunks.test.ts
@@ -3,20 +3,20 @@ import { aggregateIntoChunks } from "../app/aggregateIntoChunks";
 describe("aggregateIntoChunks testing", () => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   const result = aggregateIntoChunks(alphabet);
-  it("should return proper every chunk has more than FLOOR_NUMBER elements", () => {
+  it("should return proper when every chunk has more than FLOOR_NUMBER elements", () => {
     const doesEveryChunkHasMoreThan = result.every(
       (element) => element.length >= 5
     );
     expect(doesEveryChunkHasMoreThan).toBe(true);
   });
-  it("should return proper every chunk has less than CEIL_NUMBER elements", () => {
+  it("should return proper when every chunk has less than CEIL_NUMBER elements", () => {
     const doesEveryChunkHasLessThan = result.every(
       (element) => element.length <= 7
     );
     expect(doesEveryChunkHasLessThan).toBe(true);
   });
   describe("it returns error when", () => {
-    it("- floor Number is higher than number of elements in array", () => {
+    it("- floor number is higher than number of elements in array", () => {
       const smallArray = ["a", "b", "c", "d"];
       function expectError() {
         aggregateIntoChunks(smallArray);
